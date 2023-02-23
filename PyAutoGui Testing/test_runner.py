7391f0de3f6cc336ef6cadb87d618f60a7f63e16
Brandon Zwosta <122579115+zwos5@users.noreply.github.com>
2023-02-23 11:55:34 -0600

    Create test_runner.py

---
+++ PyAutoGui Testing/test_runner.py
@@ -0,0 +1,15 @@
+import pyautogui
+import time
+
+time.sleep(2)
+buttons = ['samplepage_home.png', 'about.png', 'purpose.png']
+for button in buttons:
+    print(f'click on {button}')
+    while pyautogui.locateOnScreen(button) is None:
+        pyautogui.PAUSE = 2.5
+    pyautogui.click(pyautogui.center(pyautogui.locateOnScreen(button)))
+    print('click back')
+    pyautogui.click(pyautogui.center(pyautogui.locateOnScreen('back.png')))
+    time.sleep(2)
+pyautogui.hotkey('alt','f4') 
+print("Test Complete")
